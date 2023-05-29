c255dc8bbe776e40d96b4e17fafb1eb613b3703c
AlexVlaso <alexvlas.javaprog@gmail.com>
2023-05-29 17:19:18 +0300

    add submit forms

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,16 @@
+import forms from "./modules/forms";
 import modals from "./modules/modal";
 import sliders from "./modules/sliders";
 
 document.addEventListener("DOMContentLoaded", () => {
   "use strict";
   modals();
-  sliders(".feedback-slider-item", "", ".main-prev-btn", ".main-next-btn");
+  sliders(
+    ".feedback-slider-item",
+    "horizontal",
+    ".main-prev-btn",
+    ".main-next-btn"
+  );
+  sliders(".main-slider-item", "vertical");
+  forms();
 });
---
+++ src/js/modules/forms.js
@@ -0,0 +1,80 @@
+const forms = (state) => {
+  const form = document.querySelectorAll("form");
+  const inputs = document.querySelectorAll("input");
+  const uploads = document.querySelectorAll("[name=upload]");
+
+  const messages = {
+    load: "Загрузка...",
+    ok: "Спасибо, мы с вами свяжемся",
+    error: "Что-то пошло не так...",
+    spinner: "./assets/img/spinner.gif",
+    success: "./assets/img/ok.png",
+    fail: "./assets/img/fail.png",
+  };
+  const clearInputs = () => {
+    inputs.forEach((item) => {
+      item.value = "";
+    });
+  };
+  const postData = async (url, data) => {
+    const res = await fetch(url, {
+      method: "POST",
+      body: data,
+    });
+    return await res.text();
+  };
+  uploads.forEach((item) => {
+    item.addEventListener("input", () => {
+      const status = item.previousElementSibling;
+      const arr = item.files[0].name.split(".");
+      const name =
+        arr[0].length > 6 ? arr[0].substring(0, 7) + "..." : arr[0] + ".";
+      status.textContent = name + arr[1];
+    });
+  });
+
+  form.forEach((item) => {
+    item.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      let message = document.createElement("div");
+      message.classList.add("status");
+
+      item.classList.add("animated", "fadeOut");
+      setTimeout(() => {
+        item.style.display = "none";
+      }, 100);
+
+      let infoImg = document.createElement("img");
+      infoImg.setAttribute("src", messages.spinner);
+
+      let infoText = document.createElement("div");
+      infoText.textContent = messages.load;
+
+      message.append(infoImg);
+      message.append(infoText);
+      item.parentNode.append(message);
+
+      const formdata = new FormData(item);
+
+      let api = item.classList.contains("form_calc")
+        ? "assets/server.php"
+        : "assets/server2.php";
+      console.log(api);
+
+      postData("assets/server.php", formdata).then((data) => {
+        console.log(data);
+        infoImg.setAttribute("src", messages.success);
+        infoText.textContent = messages.ok;
+        setTimeout(() => {
+          message.remove();
+          item.style.display = "block";
+          item.classList.remove("fadeOut");
+          item.classList.add("fadeInDown");
+        }, 5000);
+      });
+      clearInputs();
+    });
+  });
+};
+export default forms;
